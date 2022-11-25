e5852a2f69e9e14d94d03ba79d240476995c09e5
sperky7933 <sperky7933@gmail.com>
2022-11-25 10:15:32 -0500

    set up Exp1.java for experiment 1

--- Exp1.java
+++ Exp1.java
@@ -44,21 +44,24 @@ public class Exp1 {
   
   public static void main(String[] args) throws Exception {  
   
-    // not reading args[0]
-	double eps= Double.parseDouble(args[1]);
+    // choose between linear or kd
+	String method = args[0];
+
+	double eps = Double.parseDouble(args[1]);
   
     // reads the csv file
-    List<Point3D> points= Exp1.read(args[2]);
+    List<Point3D> points = Exp1.read(args[2]);
 	
-	Point3D query= new Point3D(Double.parseDouble(args[3]),
+	// read the point given
+	Point3D query = new Point3D(Double.parseDouble(args[3]),
 								Double.parseDouble(args[4]),
 								Double.parseDouble(args[5]));
 	
 	// creates the NearestNeighbor instance
-	NearestNeighbors nn= new NearestNeighbors(points);
-	List<Point3D> neighbors= nn.rangeQuery(query,eps);
+	NearestNeighbors nn = new NearestNeighbors(points);
+	List<Point3D> neighbors = nn.rangeQuery(query,eps);
 	
-	System.out.println("number of neighbors= "+neighbors.size());
+	System.out.println("number of neighbors = "+neighbors.size());
 	System.out.println(neighbors);
   }   
 }
