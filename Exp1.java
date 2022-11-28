a23042b5b7320689fd5f8f8789558183dbb6da62
sperky7933 <sperky7933@gmail.com>
2022-11-28 09:25:37 -0500

    added experiment 2 to Exp1.java

--- Exp1.java
+++ Exp1.java
@@ -14,20 +14,56 @@ import java.io.*;
 import java.util.Scanner;  
 
 public class Exp1 {
-  
-  // reads a csv file of 3D points (rethrow exceptions!)
-  public static List<Point3D> read(String filename) throws Exception {
-	  
-    List<Point3D> points= new ArrayList<Point3D>(); 
+
+	//this is Experiment 2
+	public static long averageRangequeryTimeLinear(List<Point3D> points, double eps){
+			NearestNeighbors nn = new NearestNeighbors(points);
+
+		long average = 0;
+		for(int i = 0; i < points.size() / 10; i++){
+			Point3D point = points.get(i * 10); //this should get 0, 10, 20, 30, etc...
+			long timeStart = System.nanoTime();
+			nn.rangeQuery(point,eps);
+			long timeEnd = System.nanoTime();
+
+			average += (timeEnd - timeStart) / (i + 1); //NOT SURE ABOUT THIS
+			
+		}
+
+		return average;
+	}
+	//this is Experiment 2
+	public static long averageRangequeryTimeKD(List<Point3D> points, double eps){
+		NearestNeighborsKD nn = new NearestNeighborsKD(points);
+
+		long average = 0;
+		for(int i = 0; i < points.size() / 10; i++){
+			Point3D point = points.get(i * 10); //this should get 0, 10, 20, 30, etc...
+			long timeStart = System.nanoTime();
+			nn.rangeQuery(point,eps);
+			long timeEnd = System.nanoTime();
+
+			average += (timeEnd - timeStart) / (i + 1); //NOT SURE ABOUT THIS
+			
+		}
+
+		return average;
+	}
+
+
+	// reads a csv file of 3D points (rethrow exceptions!)
+	public static List<Point3D> read(String filename) throws Exception {
+		
+	List<Point3D> points= new ArrayList<Point3D>(); 
 	double x,y,z;
-	
+
 	Scanner sc = new Scanner(new File(filename));  
 	// sets the delimiter pattern to be , or \n \r
 	sc.useDelimiter(",|\n|\r");  
 
 	// skipping the first line x y z
 	sc.next(); sc.next(); sc.next();
-	
+
 	// read points
 	while (sc.hasNext())  
 	{  
@@ -36,11 +72,11 @@ public class Exp1 {
 		z= Double.parseDouble(sc.next());
 		points.add(new Point3D(x,y,z));  
 	}   
-	
+
 	sc.close();  //closes the scanner  
-	
+
 	return points;
-  }
+	}
   
   public static void main(String[] args) throws Exception {  
   
@@ -56,21 +92,27 @@ public class Exp1 {
 	Point3D query = new Point3D(Double.parseDouble(args[3]),
 								Double.parseDouble(args[4]),
 								Double.parseDouble(args[5]));
-	
-	// creates the NearestNeighbor instance
 
+	//Linear time for EXP2
 	if (method.equals("lin")){
 		NearestNeighbors nn = new NearestNeighbors(points);
 		List<Point3D> neighbors = nn.rangeQuery(query,eps);
 		System.out.println("number of neighbors = "+neighbors.size());
 		System.out.println(neighbors);
+		long averageTime = Exp1.averageRangequeryTimeLinear(points, 0.5);
+		System.out.println("Average rangequery time for " + args[0] + " at 0.5 eps: " + averageTime);
 	}
+
+	//Linear time for EXP2
 	else{
 		NearestNeighborsKD nn = new NearestNeighborsKD(points);
 		List<Point3D> neighbors = nn.rangeQuery(query,eps);
 		System.out.println("number of neighbors = "+neighbors.size());
 		System.out.println(neighbors);
+		long averageTime = Exp1.averageRangequeryTimeKD(points, 0.5);
+		System.out.println("Average rangequery time for " + args[0] + " at 0.5 eps: " + averageTime);
 	}
+
 	/* 
 	System.out.println("number of neighbors = "+neighbors.size());
 	System.out.println(neighbors);
--- README.txt
+++ README.txt
@@ -1,1 +1,12 @@
-this is the CSI 2110 second programming assignment
+Experiments:
+
+Experiment 1 can be executed by running Exp1.
+The arguments are as follows: 
+1. lin/KD for linear or KD rangequery,
+2. EPS (distance)
+3. CSV filename
+4. X coordinate
+5. Y cooridnate
+6. Z coordinate
+
+Running experiment 1 will give the nearest neighbors. It will also give the Experiment 2 time result following the list of neighbors.
